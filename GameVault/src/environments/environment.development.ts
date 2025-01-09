84cfbeebf7c372a175ea11a8faff63a0fbf2f76e
lengyelnorbi <58424631+lengyelnorbi@users.noreply.github.com>
2025-01-09 20:22:33 +0100

    Update environment.development.ts

--- GameVault/src/environments/environment.development.ts
+++ GameVault/src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
     projectId: 'the-gamevault',
     appId: '1:294589202502:web:860056ba1aa210c7dc8a47',
     storageBucket: 'the-gamevault.appspot.com',
-    apiKey: 'AIzaSyASaXmuJ4VW7PyS3wminYkIuKbrosE0zpw',
+    apiKey: '',
     authDomain: 'the-gamevault.firebaseapp.com',
     messagingSenderId: '294589202502',
     measurementId: 'G-1KVY3NT6XE',
